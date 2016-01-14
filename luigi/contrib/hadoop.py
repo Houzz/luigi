75b82a8f2d95a517834d47a3df005e431cbf7782
Dave Buchfuhrer <buck@houzz.com>
2016-01-14 15:27:09 -0800

    Allows package binaries to be passed to job runner

--- luigi/contrib/hadoop.py
+++ luigi/contrib/hadoop.py
@@ -410,7 +410,7 @@ class HadoopJobRunner(JobRunner):
         self.end_job_with_atomic_move_dir = end_job_with_atomic_move_dir
         self.tmp_dir = False
 
-    def run_job(self, job, tracking_url_callback=None):
+    def run_job(self, job, tracking_url_callback=None, package_binary=None):
         packages = [luigi] + self.modules + job.extra_modules() + list(_attached_packages)
 
         # find the module containing the job
@@ -439,9 +439,11 @@ class HadoopJobRunner(JobRunner):
         # build arguments
         config = configuration.get_config()
         python_executable = config.get('hadoop', 'python-executable', 'python')
-        map_cmd = '{0} mrrunner.py map'.format(python_executable)
-        cmb_cmd = '{0} mrrunner.py combiner'.format(python_executable)
-        red_cmd = '{0} mrrunner.py reduce'.format(python_executable)
+        runner_arg = 'mrrunner.pex' if package_binary else 'mrrunner.py'
+        command = '{0} {1} {{step}}'.format(python_executable, runner_arg)
+        map_cmd = command.format(step='map')
+        cmb_cmd = command.format(step='combiner')
+        red_cmd = command.format(step='reduce')
 
         output_final = job.output().path
         # atomic output: replace output with a temporary work directory
@@ -497,9 +499,14 @@ class HadoopJobRunner(JobRunner):
             arglist += ['-combiner', cmb_cmd]
         if job.reducer != NotImplemented:
             arglist += ['-reducer', red_cmd]
-        files = [runner_path, self.tmp_dir + '/packages.tar', self.tmp_dir + '/job-instance.pickle']
+        packages_fn = 'mrrunner.pex' if package_binary else 'packages.tar'
+        files = [
+            mrrunner if not package_binary else None,
+            os.path.join(self.tmp_dir, packages_fn),
+            os.path.join(self.tmp_dir, 'job-instance.pickle'),
+        ]
 
-        for f in files:
+        for f in filter(None, files):
             arglist += ['-file', f]
 
         if self.output_format:
@@ -519,7 +526,10 @@ class HadoopJobRunner(JobRunner):
         arglist += ['-output', output_hadoop]
 
         # submit job
-        create_packages_archive(packages, self.tmp_dir + '/packages.tar')
+        if package_binary:
+            shutil.copy(package_binary, os.path.join(self.tmp_dir, 'mrrunner.pex'))
+        else:
+            create_packages_archive(packages, os.path.join(self.tmp_dir, 'packages.tar'))
 
         job.dump(self.tmp_dir)
 
@@ -627,6 +637,7 @@ class BaseHadoopJobTask(luigi.Task):
     final_reducer = NotImplemented
 
     mr_priority = NotImplemented
+    package_binary = None
 
     _counter_dict = {}
     task_id = None
@@ -684,7 +695,11 @@ class BaseHadoopJobTask(luigi.Task):
 
         self.init_local()
         try:
-            self.job_runner().run_job(self, tracking_url_callback=tracking_url_callback)
+            self.job_runner().run_job(
+                self,
+                tracking_url_callback=tracking_url_callback,
+                package_binary=self.package_binary,
+            )
         except TypeError as ex:
             if 'unexpected keyword argument' not in ex.message:
                 raise
