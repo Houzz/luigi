ee772b3fd1ebf2ccdb4aebb926aa36a7e5aaecdf
Dave Buchfuhrer <dbuchfuhrer@gmail.com>
2016-08-23 08:29:41 -0700

    Removes redundant deletion of S3 directory marker

--- luigi/s3.py
+++ luigi/s3.py
@@ -168,11 +168,6 @@ class S3Client(FileSystem):
         delete_key_list = [
             k for k in s3_bucket.list(self._add_path_delimiter(key))]
 
-        # Hadoop API would create _$folder$ on S3 for folders sometimes
-        if key[-1] == '/':
-            key = key[:-1]
-        delete_key_list.append("%s_$folder$" % key)
-
         # delete the directory marker file if it exists
         s3_dir_with_suffix_key = s3_bucket.get_key(key + S3_DIRECTORY_MARKER_SUFFIX_0)
         if s3_dir_with_suffix_key:
