a387c0e3469def0bfc00df7ce6b28d795db55e63
Dave Buchfuhrer <buck@houzz.com>
2016-12-06 16:08:30 -0800

    Fixes version check

--- luigi/worker.py
+++ luigi/worker.py
@@ -1057,7 +1057,7 @@ class Worker(object):
                 logger.debug('%d running tasks, waiting for next task to finish', len(self._running_tasks))
                 self._handle_next_task()
 
-            if not self.check_version():
+            if not self._check_version():
                 break
 
             get_work_response = self._get_work()
