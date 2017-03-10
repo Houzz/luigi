936b70c4e7faf27542af009e3f1478d6f317d80d
Dave Buchfuhrer <buck@houzz.com>
2017-03-09 16:17:47 -0800

    Interface bug fix

--- luigi/interface.py
+++ luigi/interface.py
@@ -204,7 +204,7 @@ def _schedule_and_run(tasks, worker_scheduler_factory=None, override_defaults=No
         logger.info('Done scheduling tasks')
         luigi_state.set_state(luigi_state.RUNNING)
         success &= worker.run()
-    if self.worker.show_execution_summary:
+    if worker.show_execution_summary:
         logger.info(execution_summary.summary(worker))
     return dict(success=success, worker=worker)
 
