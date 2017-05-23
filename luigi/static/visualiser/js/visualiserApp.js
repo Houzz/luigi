01523782b781a3b5a3e4c10f7612baaccd772f31
Dave Buchfuhrer <buck@houzz.com>
2017-05-23 16:30:09 -0700

    Remove search query from url bar on empty query

--- luigi/static/visualiser/js/visualiserApp.js
+++ luigi/static/visualiser/js/visualiserApp.js
@@ -988,6 +988,8 @@ function visualiserApp(luigi) {
 
                 if (data.search.search) {
                     state.search__search = data.search.search;
+                } else {
+                    delete state.search__search;
                 }
 
                 if (data.order && data.order.length) {
