d842923191831e5ec7a6d8ae90864222946f61dd
Dave Buchfuhrer <buck@houzz.com>
2017-05-25 14:15:41 -0700

    Store expanded resources in the URL bar

--- luigi/static/visualiser/js/visualiserApp.js
+++ luigi/static/visualiser/js/visualiserApp.js
@@ -486,6 +486,7 @@ function visualiserApp(luigi) {
             setWorkerSidebarFamilies(fragmentQuery.worker_families);
             switchTab("workerList");
         } else if (fragmentQuery.tab == "resources") {
+            expandResources(fragmentQuery.resources);
             switchTab("resourceList");
         } else if (fragmentQuery.tab == "graph") {
             var taskId = fragmentQuery.taskId;
@@ -1016,6 +1017,25 @@ function visualiserApp(luigi) {
         })
     }
 
+    function expandedResources() {
+        return $('.resource-box.in').toArray().map(function (val) { return val.dataset.resource; });
+    }
+
+    function expandResources(resources) {        
+        if (resources === undefined) {
+            resources = [];
+        } else {
+            resources = JSON.parse(resources);
+        }
+        $('.resource-box').each(function (i, item) {
+            if (resources.indexOf(item.dataset.resource) === -1) {
+                $(item).collapse('hide');
+            } else {
+                $(item).collapse('show');
+            }
+        });
+    }
+
     $(document).ready(function() {
         loadTemplates();
 
@@ -1053,6 +1073,24 @@ function visualiserApp(luigi) {
 
         luigi.getResourceList(function(resources) {
             $("#resourceList").append(renderResources(resources));
+            expandResources(URI.parseQuery(location.hash.replace('#', '')).resources);
+            $('.resources-collapse').click(function (e) {
+                e.preventDefault();
+                var collapse_block = $(this.dataset.target);
+                if (collapse_block.hasClass('collapsing')) {
+                    return;
+                }
+                var resource = collapse_block.attr('data-resource');
+                var resourceList = expandedResources();
+                var resourceIdx = resourceList.indexOf(resource);
+                if (resourceIdx === -1) {
+                    resourceList.push(resource);
+                } else {
+                    resourceList.splice(resourceIdx, 1);
+                }
+                changeState('resources', resourceList.length > 0 ? JSON.stringify(resourceList) : null);
+                collapse_block.collapse('toggle');
+            });
         });
 
         dt = $('#taskTable').DataTable({
@@ -1366,6 +1404,7 @@ function visualiserApp(luigi) {
                 }
                 state.tab = 'workers';
             } else if (tabId == 'resourceList') {
+                state.resources = JSON.stringify(expandedResources());
                 state.tab = 'resources';
             }
 
