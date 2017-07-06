d0975fee57bba8f630650b65e043e5eaa508b596
Dave Buchfuhrer <buck@houzz.com>
2017-07-06 13:15:27 -0700

    Use renderParams from config viz

--- luigi/static/visualiser/js/visualiserApp.js
+++ luigi/static/visualiser/js/visualiserApp.js
@@ -964,9 +964,10 @@ function visualiserApp(luigi) {
     function renderParams(params) {
         var htmls = [];
         for (var key in params) {
-            htmls.push(key + '=' + params[key]);
+            htmls.push('<span class="param-name">' + key +
+                '</span>: <span class="param-value">' + params[key] + '</span>');
         }
-        return htmls.join(', ');
+        return htmls.sort().join('<br>');
     }
 
     /**
